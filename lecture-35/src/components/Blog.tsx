f6644d74ddc867df96f4156e9469232e0cb8149b
Ihor Kopach <kopaclubov9@gmail.com>
2024-04-18 10:34:16 +0300

    add 35 lecture

---
+++ lecture-35/src/components/Blog.tsx
@@ -0,0 +1,17 @@
+const Blog = ({ post }: { post: any }) => {
+  return (
+    <article className="post">
+      <div className="cover-container">
+        <img src={post.cover} alt={post.title} />
+      </div>
+      <div className="post-footer">
+        <h3>
+          {post.title} {post.id}
+        </h3>
+        <p>{post.content}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Blog;
